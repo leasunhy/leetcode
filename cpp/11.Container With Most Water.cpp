bc82bcc83cf03852ce38fa1c908535c43e2fbcec
leasunhy <leasunhy@gmail.com>
2016-02-07 16:07:48 +0800

    Day 3: Container with Most Water

---
+++ cpp/11.Container With Most Water.cpp
@@ -0,0 +1,19 @@
+// Date: 2016-2-7
+
+#include <algorithm>
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res = 0;
+        int l = 0;
+        int r = height.size() - 1;
+        while (l != r) {
+            res = max(res, min(height[r], height[l]) * (r - l));
+            if (height[l] < height[r])
+                ++l;
+            else
+                --r;
+        }
+        return res;
+    }
+};
