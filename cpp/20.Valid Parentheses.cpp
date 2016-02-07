ac4b4b44fa277d985dacac4bc62595b4eb23ec88
leasunhy <leasunhy@gmail.com>
2016-02-07 15:05:14 +0800

    Day 2: Valid Parentheses

---
+++ cpp/20.Valid Parentheses.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> ps;
+        for (char c : s) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    ps.push(c);
+                    break;
+                case ')':
+                case '}':
+                case ']':
+                    if (!checkAndRemove(ps, c)) return false;
+                    break;
+            }
+        }
+        return ps.empty();
+    }
+private:
+    bool checkAndRemove(stack<char>& ps, char c) {
+        if (ps.empty()) return false;
+        if (c == ')' && ps.top() != '(') return false;
+        if (c == '}' && ps.top() != '{') return false;
+        if (c == ']' && ps.top() != '[') return false;
+        ps.pop();
+        return true;
+    }
+};
