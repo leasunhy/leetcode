b30ac597f7632245af79e8fa4a34fb2fbee202e3
leasunhy <leasunhy@gmail.com>
2016-02-07 14:55:48 +0800

    Day 2: Reverse Integer

---
+++ cpp/7.Reverse Integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        if (x == 0) return 0;
+        int sign = 1;
+        if (x < 0) {
+            sign = -1;
+            x = -x;
+        }
+        int res = 0;
+        while (x != 0) {
+            if (res > 214748364) return 0;  // overflow
+            res *= 10;
+            if (res < 0) return 0;  // overflow
+            res += x % 10;
+            if (res < 0) return 0;  // overflow
+            x /= 10;
+        }
+        return res * sign;
+    }
+};
