b58b701d5079c783a63f66d281ee746e624ecb90
leasunhy <leasunhy@gmail.com>
2016-03-15 17:32:23 +0800

    283.Move Zeroes

---
+++ cpp/283.Move Zeroes.cpp
@@ -0,0 +1,14 @@
+// Date: 2016-03-15
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i])
+                nums[j++] = nums[i];
+        while (j < nums.size())
+            nums[j++] = 0;
+    }
+};
+
