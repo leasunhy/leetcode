eb72ff20de3dad7dbddcf563b0afb6201b40e37e
leasunhy <leasunhy@gmail.com>
2016-03-15 23:19:13 +0800

    190.Reverse Bits

---
+++ cpp/190.Reverse Bits.cpp
@@ -0,0 +1,14 @@
+// Date: 2016-03-15
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0u;
+        for (int i = 0; i < 32; ++i) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+};
