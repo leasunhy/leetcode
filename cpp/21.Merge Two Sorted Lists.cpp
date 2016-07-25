d08680115ef21054554f0116b030c97c571b6309
leasunhy <leasunhy@gmail.com>
2016-07-26 01:18:46 +0800

    21.Merge Two Sorted Lists: Updated a new solution

--- cpp/21.Merge Two Sorted Lists.cpp
+++ cpp/21.Merge Two Sorted Lists.cpp
@@ -8,7 +8,35 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// this solution is in-place.
 class Solution {
+public:
+    ListNode* preInc(ListNode** l) {
+        ListNode* t = *l;
+        *l = (*l)->next;
+        return t;
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val)
+                cur->next = preInc(&l1);
+            else
+                cur->next = preInc(&l2);
+            cur = cur->next;
+        }
+        cur->next = l1 ? l1 : l2;
+        ListNode* head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
+
+// this solution constructs a new linked list.
+class Solution1 {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode* dummy = new ListNode(0);
