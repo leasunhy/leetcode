593209fb794d52ce2c4a5628b6b389804ade9661
leasunhy <leasunhy@gmail.com>
2016-07-29 21:59:50 +0800

    168.Excel Sheet Column Title

---
+++ cpp/168.Excel Sheet Column Title.cpp
@@ -0,0 +1,17 @@
+// Date: 2016-07-29
+
+#include <algorithm>
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res;
+        while (n) {
+            res.push_back((n - 1) % 26 + 'A');
+            n = (n - 1) / 26;
+        }
+        std::reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
