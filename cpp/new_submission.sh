aa17cd0da55c60007f38258797fe762de70f62fa
leasunhy <leasunhy@gmail.com>
2017-07-11 16:33:12 +0800

    update script

--- cpp/new_submission.sh
+++ cpp/new_submission.sh
@@ -11,8 +11,23 @@ PROBLEM="$NUMBER.$TITLE"
 FN="$PROBLEM.$EXT"
 
 echo -e "// Date: $(date -I)\n\n" >> "$FN"
-vim "$FN"
+
+if [[ "x$EDITOR" == "x" ]]; then
+    vim "$FN"
+else
+    $EDITOR "$FN"
+fi
+
 dos2unix "$FN"
-git add "$FN"
-git commit -m "$PROBLEM"
+
+while true; do
+    read -p "Ready to commit? [y/n]" yn
+    case $yn in
+        [Nn]* ) exit;;
+        [Yy]* ) git add "$FN";
+                git commit -e -m "$PROBLEM";
+                break;;
+        *     ) echo "Please answer yes or no."
+    esac
+done
 
